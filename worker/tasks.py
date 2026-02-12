2c8c2552821bb2f75075f4558207159436920125
Khaliknazar <sarhok001@mail.ru>
2026-02-13 00:10:26 +0500

    change redis

--- worker/tasks.py
+++ worker/tasks.py
@@ -19,6 +19,13 @@ tg = TelegramClient(BOT_TOKEN)
 oson = OsonIntelektServer()
 
 
+async def get_prompt(data: dict) -> str | None:
+    return (
+            data.get("body", {}).get("prompt")
+            or data.get("body", {}).get("input", {}).get("prompt")
+    )
+
+
 async def generate_and_send(ctx, payload: dict, user_id: int):
     redis = ctx["redis"]
 
@@ -44,9 +51,11 @@ async def generate_and_send(ctx, payload: dict, user_id: int):
             max_wait_s=120,  # if queue is huge, fail fast
         )
     except TimeoutError:
-        await tg.send_text(user_id, f"<tg-emoji emoji-id='5258474669769497337'>⚠️</tg-emoji>️ Navbat ko'p, Iltimos keginroq qayta urinib ko'ring\n\nPrompt:\n<code>{payload.get('prompt', '')}</code>")
+        await tg.send_text(user_id,
+                           f"<tg-emoji emoji-id='5258474669769497337'>⚠️</tg-emoji>️ Navbat ko'p, Iltimos keginroq qayta urinib ko'ring\n\nPrompt:\n<code>{payload.get('prompt', '')}</code>")
         return
 
+    prompt = await get_prompt(payload)
     try:
         result = await asyncio.wait_for(handler(payload), timeout=policy.timeout_s)
         logging.info(result)
@@ -55,14 +64,15 @@ async def generate_and_send(ctx, payload: dict, user_id: int):
             await tg.send_text(
                 user_id,
                 f"<tg-emoji emoji-id='5258474669769497337'>⚠️</tg-emoji>️ Yaratishda xatolik! Qayta urinib ko'ring\n{result.get('error')}.\n\nPrompt:\n"
-                f"<blockquote expandable>{payload.get('prompt', '')}</blockquote>"
+                f"<blockquote expandable>{prompt}</blockquote>"
             )
             await oson.send_job_status(payload['job_id'], 'FAILED')
             return
 
         if result.get('task_id', False):
-            await tg.send_text(user_id, f"<tg-emoji emoji-id='5258077307985207053'>©️</tg-emoji>️ {payload['media_type']} tayyornalmoqda!\n\n"
-                                        f"<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{payload['body'].get('prompt', 'kiritilmagan')}</blockquote>")
+            await tg.send_text(user_id,
+                               f"<tg-emoji emoji-id='5283112212792121487'>©️</tg-emoji>️ {payload['media_type']} tayyornalmoqda!\n\n"
+                               f"<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{prompt}</blockquote>")
             await oson.send_job_status(payload['job_id'], 'PROCESSING', task_id=result.get('task_id'))
             return
 
@@ -72,7 +82,7 @@ async def generate_and_send(ctx, payload: dict, user_id: int):
 
         await tg.send_document(user_id, filename, result["bytes"], mime, caption="@OsonIntelektBot")
         await tg.send_text(user_id, f"<tg-emoji emoji-id='5260416304224936047'>©️</tg-emoji>️ Yakunlandi!\n\n"
-                                    f"<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{payload.get('prompt', '')}</blockquote>")
+                                    f"<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{prompt}</blockquote>")
         await oson.send_job_status(payload['job_id'], 'FINISHED')
     except Exception:
         log.exception("Generation failed")
@@ -82,7 +92,8 @@ async def generate_and_send(ctx, payload: dict, user_id: int):
             log.error(f"Error sending status to server! job_id: {payload['job_id']} error: {e}")
 
         try:
-            await tg.send_text(user_id, f"<tg-emoji emoji-id='5258474669769497337'>⚠️</tg-emoji>️ Xato, Iltimos keginroq qayta urinib ko'ring.\n\n<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{payload.get('prompt', '')}</blockquote>")
+            await tg.send_text(user_id,
+                               f"<tg-emoji emoji-id='5258474669769497337'>⚠️</tg-emoji>️ Xato, Iltimos keginroq qayta urinib ko'ring.\n\n<tg-emoji emoji-id='5249231689695115145'>©️</tg-emoji>️ Prompt:\n<blockquote expandable>{prompt}</blockquote>")
         except:
             pass
         raise
--- worker/worker_settings.py
+++ worker/worker_settings.py
@@ -4,12 +4,13 @@ from worker.tasks import generate_and_send
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 
 ARQ_QUEUE = os.getenv("ARQ_QUEUE")
 
 class WorkerSettings:
-    redis_settings = RedisSettings(host=REDIS_HOST, port=REDIS_PORT, database=REDIS_DB)
+    redis_settings = RedisSettings(host=REDIS_HOST, port=REDIS_PORT, database=REDIS_DB, password=REDIS_PASSWORD )
     functions = [generate_and_send]
 
     # This worker listens to one queue (best for per-model scaling)
@@ -19,6 +20,6 @@ class WorkerSettings:
     max_jobs = int(os.getenv("ARQ_MAX_JOBS", "50"))
 
     # job timeout fallback (seconds)
-    job_timeout = int(os.getenv("ARQ_JOB_TIMEOUT", "900"))
+    job_timeout = int(os.getenv("ARQ_JOB_TIMEOUT", "300"))
 
     retry_jobs = True
